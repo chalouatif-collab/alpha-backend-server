19573fa22766b5351ae3523a7619a5799eadfea0
chalouatif-collab <chalouatif@gmail.com>
2026-09-01 20:30:38 +0100

    Update EURO_BASE_URL to production endpoint

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ PROVIDER_ENDPOINT = os.getenv("PROVIDER_ENDPOINT", "https://api.nexusggr.eu")
 # ==========================================
 EURO_APP_KEY = os.getenv("EURO_APP_KEY", "")
 EURO_API_KEY = os.getenv("EURO_API_KEY", "")
-EURO_BASE_URL = os.getenv("EURO_BASE_URL", "https://api.staging.betkraft.co.uk")
+EURO_BASE_URL = os.getenv("EURO_BASE_URL", "https://api.betkraft.co.uk/")
 
 from dotenv import load_dotenv
 import os
