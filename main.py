530210e893ab1f52e909ee9619ddc7e238dc1c2c
Fethi Chalouati <chalouatifi@egmail.com>
2026-09-22 10:36:28 +0200

    Update games endpoint to support partner_id filtering

--- main.py
+++ main.py
@@ -1149,7 +1149,7 @@ async def get_bet_history(current_user: str = Depends(get_current_user)):
     finally:
         db_session.close()
         
-        @app.get("/api/get-studios")
+@app.get("/api/get-studios")
 async def get_virtual_studios():
     try:
         payload = {}
